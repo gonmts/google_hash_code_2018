19750ee7b9901665628200bf282c57584b019dd4
Hugo Braz <hugo.braz@ist.utl.pt>
2018-02-11 16:55:15 +0000

    devolve agora os offsets

--- pizza_divider.py
+++ pizza_divider.py
@@ -4,9 +4,11 @@ def pizza_divider(pizza, x, y):
     number_of_x = pizza.shape[0] // x
     number_of_y = pizza.shape[1] // y
     subpizza = []
+    offsets = []
     for i in range(number_of_x):
         for k in range(number_of_y):
             j = i*x
             m = k*y
             subpizza += [pizza[j:j+x, m:m+y]]
-    return np.array(subpizza)
+            offsets += [(j, m)]
+    return np.array(subpizza), np.array(offsets)
