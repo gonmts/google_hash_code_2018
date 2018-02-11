1ceed4173a786880f484432d5d7bedeb679e8be0
Hugo Braz <hugo.braz@ist.utl.pt>
2018-02-11 15:43:39 +0000

    a corrigir os bugs do Goncalo

--- var_dispatcher.py
+++ var_dispatcher.py
@@ -1,23 +1,18 @@
 
 class VarDispatcher:
 
-	def __init__(self, width, height):
+	def __init__(self, height, width):
 		self.width = width
 		self.height = height
 
 
 	def pizza_to_cn(self, row, col, slic):
-		#return row + self.height*(col + self.width*slic)
-		return (row*self.width + col)*self.heigh + slic
-		#return slic*self.width*self.height + col*self.width + row
+		return (slic*self.width*self.height) + row*self.width + col
 
 
 
 	def cnf_to_max(self, var):
-		row = var % self.height
-		col = (var - row)/self.height % self.width
-		slic = ((var - row)/self.height - col) / self.width
-		#row = var % self.width
-		#col = (var - row)/self.widht  % self.height
-		#slic = ((var - row)/self.width - col) / self.height
+		slic = var // (self.width * self.height)
+		col = (var - slic * (self.width * self.height)) // self.width
+		row = (var - slic * (self.width * self.height)) - (col * self.width)
 		return (row, col, slic)
