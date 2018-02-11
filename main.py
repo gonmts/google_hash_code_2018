638324bd779c68081dacad96a033a8b1b72da995
Hugo Braz <hugo.braz@ist.utl.pt>
2018-02-11 17:11:47 +0000

    restricao cenas

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import pseudo_boolean
 
 class Main:
     def __init__(self):
-        
+
     def constraint_minL():
         cnf_tomato=[]
         cnf_mushroom=[]
@@ -24,35 +24,39 @@ class Main:
                         cnf_tomato+=self.var_dispatcher.pizza_to_cnf(i,j,par_slice)
                     else:
                         cnf_mushroom+=self.var_dispatcher.pizza_to_cnf(i,j,par_slice)
-        
-        #cnf_tomato.append()
-                        
-        s_ij_tomato=pseudo_boolean.PBConstraintGEQ(cnf_tomato, [1] * len(cnf_tomato), self.l , self.solver) 
-        s_ij_mushroom=pseudo_boolean.PBConstraintGEQ(cnf_tomato, [1] * len(cnf_tomato),  self.l, self.solver)
-              
-   
-                        
 
-                 
-            
-        
-        
-        
-        
-    
+        #cnf_tomato.append()
+
+        s_ij_tomato=pseudo_boolean.PBConstraintGEQ(cnf_tomato, [1] * len(cnf_tomato), self.l , self.solver)
+        s_ij_mushroom=pseudo_boolean.PBConstraintGEQ(cnf_tomato, [1] * len(cnf_tomato),  self.l, self.solver)
+
+    def contraint_maxCell():
+        var = []
+        for i in range(self.rows):
+            for j in range(self.col):
+                for s in range(self.par_slice):
+                    var += [self.var_dispatcher.pizza_to_cnf(i, j, s)]
+                s_ij_cenas=pseudo_boolean.PBConstraintLEQ(var, [1] * len(var), 1, self.solver)
+
+
+
+
+
+
+
+
+
     def main(self, filename):
         self.rows, self.col, self.l, self.h, self.pizza=read_pizza.read_pizza_file(filename)
         self.par_slice=(rows*col)/max_cell
         self.solver=Solver()
         self.var_dispatcher= VarDispatcher(rows, col,par_slice,solver)
-    
-    
-    
-    
+
+
+
+
 
 
 
 
 if __name__ == "__main__":
-    
-    
