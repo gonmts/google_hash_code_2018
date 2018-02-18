1262caa1a89122b172be97a8fd27b7519fcaa246
Hugo Braz <hugo.braz@ist.utl.pt>
2018-02-18 13:29:50 +0000

    correcoes e ja da solucoes fixes

--- videos/main.py
+++ videos/main.py
@@ -8,42 +8,41 @@ class main:
 
 
 	def pontuacao_endpoint_video(self, requests, latency):
-		return request*latency
+		return requests*latency
 
 
 
 	def main(self):
-		fname = 'datasets/me_at_the_zoo.in'
-		load_input(fname)
+		fname = 'datasets/kittens.in.txt'
+		self.load_input(fname)
 
 		#Main algorithm
 
 		# 1 + 2
 		endpoint_lists = {}
-		gloibal_list = []
+		global_list = []
 		for endpoint in range(self.nrEndpoints):
 			endpoint_lists[endpoint] = []
 			for video in self.videoRequests[endpoint]:
-				score = pontuacao_endpoint_video( video[1], self.endpoints[endpoint][0] )
+				score = self.pontuacao_endpoint_video( video[1], self.endpoints[endpoint][0] )
 				endpoint_lists[endpoint] += [(video, score)] # [ (video, score) ]
-				global_list += [(video, endpoint, score)]
-			endpoint_lists[endpoint] = sorted(endpoint_lists[endpoint], key=getkey)
+				global_list += [(video[0], endpoint, score)]
+			endpoint_lists[endpoint] = sorted(endpoint_lists[endpoint], key=getkey, reverse=True)
 
 
 		# 3
-		global_list = sorted(global_list, key=getkey2)
+		global_list = sorted(global_list, key=getkey2, reverse=True)
 
 		# 4
-		tratado = false
 		for video_object in global_list:
+			tratado = False
 			video = video_object[0]
 			endpoint = video_object[1]
 			score = video_object[2]
-
 			for epcache in self.endpoints[endpoint][1:]:
 				epcache = epcache[0]
-				if (video in self.caches[cache]):
-					tratado = true
+				if (video in self.caches[epcache]):
+					tratado = True
 					break
 
 			if(tratado):
@@ -51,20 +50,16 @@ class main:
 
 			sorted_ep_caches = sorted(self.endpoints[endpoint][1:], key=getkey)
 			for epcache in sorted_ep_caches:
-				if(has_capacity(epcache, video)):
-					insert_cache(epcache, video)
+				if(self.has_capacity(epcache[0], video)):
+					self.insert_cache(epcache[0], video)
 					break
-
-
-
-
 
 	def has_capacity(self, cache, video):
 		return self.cacheCapacity[cache] >= self.videoSize[video]
 
 	def insert_cache(self, cache, video):
 		self.caches[cache].add(video)
-		self.cacheCapacity[cache] -= self.videoSize(video)
+		self.cacheCapacity[cache] -= self.videoSize[video]
 
 
 
@@ -72,6 +67,7 @@ class main:
 		self.nrVideos, self.nrEndpoints, self.requests, self.nrCaches, self.cacheSize, self.videoSize, self.endpoints, self.videoRequests = read_videos_file(fname)
 
 		self.caches = {}
+		self.cacheCapacity = {}
 		for cache in range(self.nrCaches):
 			self.caches[cache] = set()
 			self.cacheCapacity[cache] = self.cacheSize
