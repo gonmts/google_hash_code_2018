5a9475a3c9f52622df0797070acc1a8ae1a2c240
Hugo Braz <hugo.braz@ist.utl.pt>
2018-02-25 17:13:21 +0000

    adicionados args no construtor

--- satellites/cImage.py
+++ satellites/cImage.py
@@ -1,6 +1,6 @@
 class cImage:
 
-	def __init__(self):
-		self.pos = (0,0) #img position on globe with lagitude and longitude
-		self.time = [(0,0),(0,0)] #time frames that the image can be taken
-		self.score = 0 #heuristic score of the image
+	def __init__(self, pos, time, score):
+		self.pos = pos #img position on globe with lagitude and longitude
+		self.time = time #time frames that the image can be taken
+		self.score = score #heuristic score of the image
