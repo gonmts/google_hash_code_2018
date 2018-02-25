3bceb8fc199d31c5d93bed9a7ccfe16e9357f937
Hugo Braz <hugo.braz@ist.utl.pt>
2018-02-25 18:32:26 +0000

    adicionei funcao que devolve lista de fotos num determinado local

--- satellites/calc_score.py
+++ satellites/calc_score.py
@@ -9,3 +9,20 @@ def calc_score(collection):
         if all_taken:
             score += (collection[key][0] * len(collection[key]))
     return score
+
+def is_in_time(photo_turn, conditions):
+    for cond in conditions:
+        if photo_turn >= cond[0] and photo_turn <= cond[1]:
+            return True
+    return False
+
+def list_photos_in_range(satellite, position, list_photos, time):
+    photos_dict = {}
+    for photo in list_photos:
+        if satellite.cenas(position, photo) and is_in_time(time, photo.time):
+            if photo.pos not in photos_dict:
+                photos_dict[photo.pos] = [0]
+            photos_dict[photo.pos][0] += photo.score
+            photos_dict[photo.pos] += [photo]
+
+    return sorted(photos_dict.values(), key=lambda x: x[0])
