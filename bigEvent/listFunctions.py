a0acaa2d608817e71286d19381a9dd55efe67af1
Hugo Braz <hugo.braz@ist.utl.pt>
2018-03-01 19:18:08 +0000

    correcao bug

--- bigEvent/listFunctions.py
+++ bigEvent/listFunctions.py
@@ -3,5 +3,6 @@ from random import randint
 
 def getRandomListIndex(list, maxEl):
 	lstLength = len(list)
-	index = randint(0, lstLength - maxEl)
-	return index
+	if (lstLength == maxEl):
+		return 0
+	return randint(0, lstLength - maxEl)
