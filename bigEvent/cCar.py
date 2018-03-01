c13d74c311d38e85d74996335b18ed516947a7b5
Hugo Braz <hugo.braz@ist.utl.pt>
2018-03-01 19:02:50 +0000

    funcao escrita ficheiros

--- bigEvent/cCar.py
+++ bigEvent/cCar.py
@@ -1,14 +1,16 @@
 from cTrip import *
 
 class cCar:
-	def __init__(self):
+	def __init__(self, id):
 		self.current_pos = (0,0) #tuple
 		self.onRide = False #bool
-		self.destination = False #tuple 
+		self.destination = False #tuple
+		self.completed_trips = [] #list
 
 	def beginTrip(self, cTrip):
 		onRide = True
 		self.destination = cTrip.endPoint
+		self.completed_trips += [cTrip]
 
 
 	def endTrip(self):
