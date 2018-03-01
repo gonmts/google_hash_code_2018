0ae3ca6b275daf87b56bb0247175b0f1a84ae24a
Gonçalo Matos <goncalo.p.matos@ist.utl.pt>
2018-03-01 18:40:02 +0000

    Class cTrip with construcyot

---
+++ bigEvent/cTrip.py
@@ -0,0 +1,11 @@
+class cTrip:
+
+	def __init__(self, startPoint, endPoint, earliestStart, latestFinish):
+		self.startPoint = startPoint
+		self.endPoint = endPoint
+		self.earliestStart = earliestStart
+		self.latestFinish = latestFinish
+
+
+	
+
